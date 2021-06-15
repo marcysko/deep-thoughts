ccbf52fa64815a89569c0e89587f02c201673afd
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-15 16:12:39 -0700

    add react app files

---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+
+const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+console.log(thoughts);
+
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
